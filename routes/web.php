cebcc9f971ae70460ca43c568f68da4fdd9f87bf
Ish Sookun <ish.sookun@lasentinelle.mu>
2025-04-06 12:16:14 +0400

    Cleared blade files

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-
-Route::get('/old', function () {
-    return view('old');
-});
-
-Route::get('/test', function () {
-    return view('test');
-});
