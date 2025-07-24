fd3f322aab293e0df4b305e5c45675a0f08ba5be
Ish Sookun <ish.sookun@lasentinelle.mu>
2025-07-24 13:47:38 +0400

    Updated Photo album

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::get('/speakers', [SpeakersController::class, 'index'])->name('speakers');
 Route::get('/speaker/{id}', [SpeakerController::class, 'searchById'])->name('speaker');
 
 // Photo Album
-Route::redirect('/photos', 'https://photos.google.com/share/AF1QipMxOy3GNTMyJAITPiw7L-Cd9GbzzFkdpH6d9JQTeL8QhlZg8vKhhDi__mfOQ6GZMQ', 301);
+// Route::redirect('/photos', 'https://photos.google.com/share/AF1QipMxOy3GNTMyJAITPiw7L-Cd9GbzzFkdpH6d9JQTeL8QhlZg8vKhhDi__mfOQ6GZMQ', 301);
+Route::redirect('/photos', 'https://photos.app.goo.gl/7eP1bQubGdNDDkPf8', 301);
